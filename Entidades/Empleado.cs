f9bfb9600c355265daaf13118c4a752078d53de6
JAOLMOS07 <jhogerolmos10@gmail.com>
2023-03-27 10:56:08 -0500

    Entidades Creadas

---
+++ Entidades/Empleado.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    internal class Empleado: Persona
+    {
+
+        String cargo { get; set; }
+        String contrasena { get; set; }
+        public Empleado(string cedula, string nombre, string telefono,String cargo,String contrasena) : base(cedula, nombre, telefono)
+        {
+            this.cargo = cargo;
+            this.contrasena = contrasena;
+        }
+
+
+    }
+}
---
+++ Entidades/Persona.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    internal class Persona
+    {
+
+
+        String cedula { get; set; }
+        String nombre { get; set; }
+        String telefono { get; set; }
+        public Persona(string cedula, string nombre, string telefono)
+        {
+            this.cedula = cedula;
+            this.nombre = nombre;
+            this.telefono = telefono;
+        }
+
+    }
+}
